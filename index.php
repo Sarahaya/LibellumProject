01064b8c7ecad0b5c7b5990fc114cbcf0aeaf98f
Mateus Oliveira <olivera.douge@gmail.com>
2018-06-23 15:36:11 -0300

    Remoção dos "+" indesejados

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-+<?php
-+//Constantes para definição do caminho até o MVC
-+define("MVC_MODEL","model");//Define o local da pasta model
-+define("MVC_CONTROLLER","controller");//Define o local da pasta controller
-+define("MVC_VIEW","view");//Define o local da pasta view
-+define("DEFAULT_CONTROLLER","contSite");
-+define("PATH_LIBS",'/var/www/html/libs');
-+
-+include MVC_MODEL."/controller.php";
-+
-+new controller();
+<?php
+//Constantes para definição do caminho até o MVC
+define("MVC_MODEL","model");//Define o local da pasta model
+define("MVC_CONTROLLER","controller");//Define o local da pasta controller
+define("MVC_VIEW","view");//Define o local da pasta view
+define("DEFAULT_CONTROLLER","contSite");
+define("PATH_LIBS",'/var/www/html/libs');
+
+include MVC_MODEL."/controller.php";
+
+new controller();
